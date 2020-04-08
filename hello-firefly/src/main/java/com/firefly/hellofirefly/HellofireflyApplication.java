9f3cd7c66a9e37f7f06c8d459565f33c0f6ccbed
qiuchunwei <qiuchunwei618@163.com>
2020-04-08 22:14:44 +0800

    springboot2 hello firefly

---
+++ hello-firefly/src/main/java/com/firefly/hellofirefly/HellofireflyApplication.java
@@ -0,0 +1,13 @@
+package com.firefly.hellofirefly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellofireflyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HellofireflyApplication.class, args);
+    }
+
+}
---
+++ hello-firefly/src/test/java/com/firefly/hellofirefly/HellofireflyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firefly.hellofirefly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellofireflyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
